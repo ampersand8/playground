2335a80b160467da91e27d342eb11454f1390241
Simon <ampersand8@gmail.com>
2014-06-04 23:21:57 +0200

    Create file simplestring.c

---
+++ c/simplestring.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char * myhobby = "C programming";
+    char myworld[] = "computers";
+    printf("Hi, my hobby is %s and I live in the world of %s\n",myhobby,myworld);
+    return 0;
+}
