47472334d91cddf65d85ff4d5301c6c1304baa7b
Simon <ampersand8@gmail.com>
2014-06-04 23:24:30 +0200

    Add comments

--- c/simplestring.c
+++ c/simplestring.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char * myhobby = "C programming";
+    /* Define string with a pointer */
+    char * myhobby = "C programming"
+;
+    /* Define string with a char array */
     char myworld[] = "computers";
+
     printf("Hi, my hobby is %s and I live in the world of %s\n",myhobby,myworld);
     return 0;
 }
