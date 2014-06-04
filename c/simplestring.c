eebe1205053b6c2b05c76940113a801856cb2e2e
Simon <ampersand8@gmail.com>
2014-06-04 23:36:04 +0200

    Add my first if statement with string comparison

--- c/simplestring.c
+++ c/simplestring.c
@@ -2,10 +2,16 @@
 
 int main() {
     /* Define string with a pointer */
-    char * myhobby = "C programming"
-;
+    char * myhobby = "C programming";
+
     /* Define string with a char array */
     char myworld[] = "computers";
+
+    /* Comparing a string */
+    if(strncmp(myworld,"computers") != 0) {
+        printf("Sorry, that's the wrong world.\n");
+        return 1;
+    }
 
     printf("Hi, my hobby is %s and I live in the world of %s\n",myhobby,myworld);
     return 0;
