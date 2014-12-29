567fd2b40d49268e494021985ab59973b52d2e8b
ampersand8 <ampersand8@gmail.com>
2014-12-29 22:55:58 +0100

    some new app routes

--- python/flaskr/flaskr.py
+++ python/flaskr/flaskr.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -11,3 +12,33 @@ PASSWORD = 'default'
 # create our application
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql',mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g,'db',None)
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    app.run()
